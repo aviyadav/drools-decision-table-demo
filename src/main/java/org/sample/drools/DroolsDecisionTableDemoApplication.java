2925eec6ae202f9d1accd7fe0366c5fe80b1c832
Avinash Yadav <aviyadav@users.noreply.github.com>
2022-01-12 16:59:46 +0530

    adding drools-decision-table-demo

---
+++ src/main/java/org/sample/drools/DroolsDecisionTableDemoApplication.java
@@ -0,0 +1,13 @@
+package org.sample.drools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroolsDecisionTableDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroolsDecisionTableDemoApplication.class, args);
+	}
+
+}
